ab8a07738d4b7120d6a4baedd3719ceea1d854e8
HUGO LYRA MARTINS VIANA <hugo.viana1@alunos.sc.senac.br>
2024-10-30 20:43:19 -0300

    adicao da multiplicacao

---
+++ SuperCalculadora/Multiplicacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperCalculadora
+{
+    public class Multiplicacao
+    {
+        public double Multiplicar(double nmr1, double nmr2)
+        {
+            return nmr1 * nmr2;
+        }
+    }
+}
