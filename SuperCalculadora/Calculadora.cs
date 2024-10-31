50c91115ba6de3e0713bc495cfd40ac4c1551223
BRUNO DE MATOS <bruno.matos1@alunos.sc.senac.br>
2024-10-30 21:30:37 -0300

    interface atualizada

--- SuperCalculadora/Calculadora.cs
+++ SuperCalculadora/Calculadora.cs
@@ -11,9 +11,52 @@ namespace SuperCalculadora
         static void Main(string[] args)
         {
             Console.WriteLine("Bem vindo a Super Calculadora");
-            Console.WriteLine("Digite numeros e operações. (ENTER para sair)");
+            Console.WriteLine(" + para somar.");
+            Console.WriteLine(" - para subtrair.");
+            Console.WriteLine(" * para mutiplicar.");
+            Console.WriteLine(" / para dividir.");
+            Console.WriteLine("(ENTER para sair)");
+
+            Console.WriteLine("Informe o primeiro numero");
+            double nmr1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a operação desejada");
+            string operacao = Console.ReadLine();
+
+            Console.WriteLine("Informe o segundo numero");
+            double nmr2 = double.Parse(Console.ReadLine());
+
+            double somaTotal = 0;
+
+            switch (operacao)
+            {
+                case "+" :
+                    {
+                        Soma soma = new Soma();
+                        somaTotal = soma.Somar(nmr1, nmr2);
+                        break;
+                    }
+                case "-":
+                    {
+
+                        break;
+                    }
+                case "*":
+                    {
+
+                        break;
+                    }
+                case "/":
+                    {
+                        Divisao divisao = new Divisao();
+                        somaTotal = divisao.Dividir(nmr1, nmr2);
+                        break;
+                    }
+            }
+
+
         }
+
+
     }
-
-
 }
